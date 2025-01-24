b94439a41e085a14e8dd692b610f5d1016d9856c
littlebox-spb <altukhov.alexandr@mail.ru>
2025-01-24 10:16:37 +0300

    modified:   .gitignore 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-//import reactRefresh from "@vitejs/plugin-react-refresh";
+// import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vite.dev/config/
 export default defineConfig({
